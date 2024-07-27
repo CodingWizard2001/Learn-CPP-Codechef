ac0419ffb383acba5a465ae923809e02d4d2dbde
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-07-27 09:39:57 +0530

    Modified Null Pointer problem

--- Pointers/NULLPointer.cpp
+++ Pointers/NULLPointer.cpp
@@ -2,3 +2,16 @@
 
 
 
+#include <iostream>
+using namespace std;
+
+void printValue(int *ptr) {
+    if (ptr != nullptr) cout << "Value: " << *ptr << endl; 
+    else cout << "Pointer is null" << endl;
+}
+
+int main() {
+    int *ptr = nullptr; 
+    printValue(ptr);
+    return 0;
+}
